80fede1e47015d25b621e4ade5d8efd744ceba01
Alisson-Frota-Soares <alissonfrotasoares@gmail.com>
2020-05-23 15:08:39 +0100

    react native camera instalado teste de codigo de barras

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+import RCTCamera, {RNCamera} from "react-native-camera";
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+
+  render() {
+    return (
+      <View>
+        <RNCamera style={{width:'100%', height: '100%'}}
+          onBarCodeRead={() => alert("ola")}
+        
+        />
+      </View>
+    );
+  }
+}
