a415f25cc8ac7792aa1baa18d6274d3df1f954ce
Alisson-Frota-Soares <alissonfrotasoares@gmail.com>
2020-05-28 15:09:16 +0100

    adicionado sistema de cards adicionado sistema de selecionar cards adicionado sistema para aumentar a quantidade do produto desgin dos cards ainda nao concluido

--- src/pages/home.js
+++ src/pages/home.js
@@ -3,10 +3,11 @@ import {
     View,
     Text,
     StyleSheet,
-    TouchableOpacity
+    TouchableOpacity,
+    FlatList,
 } from 'react-native';
 
-import { Header, Left, Right, Body } from "native-base";
+import { Header, Left, Right, Body, Title, Radio } from "native-base";
 
 import IconMaterial from 'react-native-vector-icons/MaterialCommunityIcons'
 import IconIonicons from 'react-native-vector-icons/Ionicons'
@@ -16,38 +17,210 @@ export default class home extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            produtos: []
+            produtos: [
+                {
+                    produto: "leite",
+                    marca: "italac",
+                    id: "231434",
+                    caracteristicas: [],
+                    preco: 1,
+                    quantidade: 1,
+                    image: null,
+                    selected: false
+                },
+                {
+                    produto: "banana",
+                    marca: "italac",
+                    id: "33223",
+                    caracteristicas: [],
+                    preco: 1,
+                    quantidade: 1,
+                    image: null,
+                    selected: false
+                },
+                {
+                    produto: "biscoito",
+                    marca: "italac",
+                    id: "33",
+                    caracteristicas: [],
+                    preco: 1,
+                    quantidade: 1,
+                    image: null,
+                    selected: false
+                },
+                {
+                    produto: "choriço",
+                    marca: "italac",
+                    id: "223",
+                    caracteristicas: [],
+                    preco: 1,
+                    quantidade: 1,
+                    image: null,
+                    selected: false
+                }
+            ],
+            isSelecting: false
         };
     }
+
+    card(item, index) {
+
+        return (
+            <TouchableOpacity style={!item.selected ? { flexDirection: "row", justifyContent: "space-between", borderBottomWidth: 1 } : { flexDirection: "row", justifyContent: "space-between", backgroundColor: "#abd4de", borderBottomWidth: 1 }}
+                onLongPress={() => {
+
+                    item.selected = item.selected ? false : true
+                    this.isSelecting()
+
+                }}
+
+                onPress={() => {
+                    if (this.state.isSelecting) {
+                        item.selected = item.selected ? false : true
+                        this.isSelecting()
+                    } else {
+
+                    }
+                }}
+            >
+
+
+                {this.state.isSelecting ?
+                    <View style={{flex: 0.5, justifyContent: "center", alignItems: "center" }}>
+
+
+
+                        <Radio selected={item.selected}
+                            onPress={() => {
+
+                                item.selected = item.selected ? false : true
+                                this.isSelecting()
+
+
+                            }}
+                            color="#0088a9"
+                            selectedColor="#0088a9"
+                        />
+
+
+                    </View>
+                    : null
+                }
+                <View style={{ flex: 1 }} >
+                    {//imagem
+                    }
+                </View>
+                <View style={{ flex: 2 }}>
+                    <Text>{item.produto}</Text>
+                    <Text>{item.caracteristicas}</Text>
+                    <View style={{ flexDirection: "row" }}>
+
+                        <TouchableOpacity
+                            style={styles.btnsMoreLess}
+                            onPress={() => { item.quantidade++; this.forceUpdate() }}
+                        >
+                            <Text style={styles.TextMoreLess}>+</Text>
+                        </TouchableOpacity>
+
+                        <View style={styles.quantView}><Text>{item.quantidade}</Text></View>
+
+                        <TouchableOpacity
+                            style={styles.btnsMoreLess}
+                            onPress={() => { if(item.quantidade > 1){item.quantidade--; this.forceUpdate()} }}                            
+                        >
+                            <Text style={styles.TextMoreLess}>-</Text>
+                        </TouchableOpacity>
+                    </View>
+                </View>
+
+                <View style={{ flex: 0.5 }}>
+                    <Text>{item.preco}€</Text>
+                </View>
+            </TouchableOpacity>
+        )
+    }
+
+    leftHeader() {
+
+        if (this.state.isSelecting == true) {
+
+            return (
+                <TouchableOpacity>
+                    <IconIonicons name="md-trash" size={30} color="#0088a9" />
+                </TouchableOpacity>
+            )
+
+
+        } else if (!this.state.produtos.length == 0) {
+            return (
+                <TouchableOpacity style={styles.Left} >
+                    <Title style={[styles.txtHeader, { fontSize: 18 }]}>
+                        FINALIZAR
+                    </Title>
+                </TouchableOpacity>)
+        }
+
+
+    }
+
+    isSelecting() {
+
+        let totalSelected = 0;
+
+        this.state.produtos.forEach(element => {
+            if (element.selected) {
+                totalSelected++;
+
+                return;
+            }
+        });
+
+        if (totalSelected > 0) {
+            this.setState({ isSelecting: true })
+        } else {
+            this.setState({ isSelecting: false })
+        }
+
+    }
+
+
+
 
     render() {
         return (
             <View style={{ flex: 1 }}>
                 <Header androidStatusBarColor="#aaa" style={styles.Header}>
-                    <Left>
+                    <Left style={{ flex: 1 }}>
                         <TouchableOpacity style={styles.Left} onPress={() => this.props.navigation.openDrawer()}>
                             <IconMaterial name="menu" size={30} />
                         </TouchableOpacity>
 
                     </Left>
-                    <Body>
-                        <Text style={styles.txtTotal}>00€</Text>
+                    <Body style={{ flex: 1 }}>
+                        <Title style={styles.txtHeader}>
+                            00€
+                        </Title>
                     </Body>
-                    <Right>
-                        <TouchableOpacity style={styles.Left} >
-                            <IconIonicons name="ios-more" size={30} />
-                        </TouchableOpacity>
+                    <Right style={{ flex: 1 }}>
+                        {
+                            this.leftHeader()
+                        }
 
 
                     </Right>
                 </Header>
 
 
+                <FlatList
+                    data={this.state.produtos}
+                    renderItem={({ item, index }) => this.card(item, index)}
+                    keyExtractor={(item) => item.id}
+                />
 
 
 
                 <TouchableOpacity activeOpacity={0.5} style={styles.floatButton}
-                    onPress={() => this.props.navigation.navigate("scanear") }
+                    onPress={() => this.props.navigation.navigate("scanear")}
                 >
                     <IconIonicons name="ios-barcode" size={35} color="#fff" />
                 </TouchableOpacity>
@@ -59,7 +232,8 @@ export default class home extends Component {
 
 const styles = StyleSheet.create({
     Header: {
-        backgroundColor: "#e0e0e0"
+        backgroundColor: "#e0e0e0",
+        justifyContent: "space-between"
     },
     left: {
         width: 50,
@@ -78,9 +252,28 @@ const styles = StyleSheet.create({
         alignSelf: "center",
         backgroundColor: "#0088a9",
     },
-    txtTotal: {
+    txtHeader: {
+        color: "#0088a9",
+        fontWeight: "bold",
+        alignSelf: "center"
 
-        textAlign: "center"
+    },
+    btnsMoreLess: {
+        backgroundColor: "#0088a9",
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center"
+    },
+    TextMoreLess: {
+        color: "#fff",
+        fontWeight: "bold",
+        fontSize: 18
+    },
+    quantView: {
+        borderWidth: 1,
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center"
     }
 
 });
--- src/pages/scanCode.js
+++ src/pages/scanCode.js
@@ -8,6 +8,8 @@ import {
 
 import { RNCamera } from 'react-native-camera'
 
+import Sound from 'react-native-sound';
+
 export default class scanCode extends Component {
     constructor(props) {
         super(props);
@@ -15,20 +17,97 @@ export default class scanCode extends Component {
         };
     }
 
+    componentDidMount() {
+
+    }
+
+    tocarBeep() {
+        Sound.setCategory('Playback');
+
+        // Load the sound file 'whoosh.mp3' from the app bundle
+        // See notes below about preloading sounds within initialization code below.
+        var whoosh = new Sound('beep.wav', Sound.MAIN_BUNDLE, (error) => {
+            if (error) {
+                console.log('failed to load the sound', error);
+                return;
+            }
+            // loaded successfully
+            console.log('duration in seconds: ' + whoosh.getDuration() + 'number of channels: ' + whoosh.getNumberOfChannels());
+
+            // Play the sound with an onEnd callback
+            whoosh.play((success) => {
+                if (success) {
+                    console.log('successfully finished playing');
+                } else {
+                    console.log('playback failed due to audio decoding errors');
+                }
+            });
+        });
+
+        // Reduce the volume by half
+        whoosh.setVolume(0.5);
+
+        // Position the sound to the full right in a stereo field
+        whoosh.setPan(1);
+
+        // Loop indefinitely until stop() is called
+        whoosh.setNumberOfLoops(-1);
+
+        // Get properties of the player instance
+        console.log('volume: ' + whoosh.getVolume());
+        console.log('pan: ' + whoosh.getPan());
+        console.log('loops: ' + whoosh.getNumberOfLoops());
+
+        // Seek to a specific point in seconds
+        whoosh.setCurrentTime(2.5);
+
+        // Get the current playback point in seconds
+        whoosh.getCurrentTime((seconds) => console.log('at ' + seconds));
+
+        // Pause the sound
+        whoosh.pause();
+
+        // Stop the sound and rewind to the beginning
+        whoosh.stop(() => {
+            // Note: If you want to play a sound after stopping and rewinding it,
+            // it is important to call play() in a callback.
+            whoosh.play();
+        });
+
+        // Release the audio player resource
+        whoosh.release();
+    }
+
     render() {
         return (
             <View style={{ flex: 1 }}>
 
                 <RNCamera style={styles.cameraView}
-                
-                    onBarCodeRead={(code)=> alert(code.data) }
+
+                    onBarCodeRead={(code) => this.tocarBeep()}
                 />
-                {/*
-                <View style={styles.maskCam}>
-                    <View></View>
-                    <View></View>
-                    <View></View>
-                </View>*/}
+
+                <View style={styles.mask}>
+                    <View style={styles.maskUpDown}></View>
+                    <View style={styles.maskMiddle}>
+                        <View style={styles.maskMSides}></View>
+
+                        <View style={styles.maskMM}>
+                            <View style={{justifyContent:"space-between"}}>
+                                <View style={[styles.square, {borderTopWidth:3, borderLeftWidth:3 ,borderColor:"#fff"}]} />
+                                <View style={[styles.square, {borderLeftWidth:3,borderBottomWidth: 3,borderColor:"#fff"}]} />
+                            </View>
+
+                            <View style={{justifyContent:"space-between"}}>
+                                <View style={[styles.square, {borderTopWidth:3, borderRightWidth:3 ,borderColor:"#fff"}]} />
+                                <View style={[styles.square, {borderRightWidth:3, borderBottomWidth:3 ,borderColor:"#fff"}]} />
+                            </View>
+
+                        </View>
+                        <View style={styles.maskMSides}></View>
+                    </View>
+                    <View style={styles.maskUpDown}></View>
+                </View>
 
             </View>
         );
@@ -42,14 +121,45 @@ const styles = StyleSheet.create({
     cameraView: {
         flex: 1,
     },
-    maskCam: {
+    mask: {
         flex: 1,
-        backgroundColor: "#ff0000",
         position: "absolute",
-        top:0,
-        left:0,
-        width:"100%",
-        height:"100%",
+        top: 0,
+        left: 0,
+        width: "100%",
+        height: "100%",
+    },
+    maskUpDown: {
+        flex: 2,
+        backgroundColor: "#000",
         opacity: 0.5,
+
+    },
+    maskMiddle: {
+        flex: 1.3,
+        flexDirection: "row",
+
+    },
+    maskMSides: {
+        flex: 1,
+        backgroundColor: "#000",
+        opacity: 0.5,
+    },
+    maskMM: {
+        flex: 10,
+        flexDirection: "row",
+        justifyContent: "space-between"
+    },
+    square: {
+        width: 50,
+        height: 50,
     }
+
 });
+
+
+
+
+
+
+
