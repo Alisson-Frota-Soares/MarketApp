328750cff8d4b863330bf01f8a06a103093d29b6
Alisson-Frota-Soares <alissonfrotasoares@gmail.com>
2020-05-30 14:06:20 +0100

    lista com os produtos movida para um arquivo externo

---
+++ src/produtos.js
@@ -0,0 +1,49 @@
+const produtos = [{
+    produto: "leite",
+    marca: "italac",
+    id: "231434",
+    caracteristicas: [
+        "500",
+        "ml"
+    ],
+    preco: 0.99,
+    quantidade: 1,
+    image: { uri: "https://www.recheio.pt/catalogo/media/catalog/product/cache/1/image/900x900/9df78eab33525d08d6e5fb8d27136e95/8/0/80908_4.png" },
+    selected: false
+},
+{
+    produto: "banana",
+    marca: "banana tem marca?",
+    id: "33223",
+    caracteristicas: [
+        "250",
+        "g"
+    ],
+    preco: 0.44,
+    quantidade: 1,
+    image: { uri: "https://oxfam.org.br/wp-content/uploads/2019/08/banana-terra-1024x1024.png" },
+    selected: false
+},
+{
+    produto: "biscoito",
+    marca: "passatempo",
+    id: "33",
+    caracteristicas: ["200", "g"],
+    preco: 1.13,
+    quantidade: 1,
+    image: { uri: "https://static.wixstatic.com/media/794a04_ceafc9aaabde400ea3e2a1826caba6be~mv2.png/v1/fill/w_238,h_238,al_c,q_85,usm_0.66_1.00_0.01/794a04_ceafc9aaabde400ea3e2a1826caba6be~mv2.webp" },
+    selected: false
+},
+{
+    produto: "choriço",
+    marca: "italac",
+    id: "223",
+    caracteristicas: ["300", "g"],
+    preco: 0.66,
+    quantidade: 1,
+    image: { uri: "https://7bellotas.com/426-large_default/longaniza-iberica.jpg" },
+    selected: false
+}];
+
+
+export default produtos;
